dc9ed7eedad005e80294e9c02eaa3a602c1f9811
Roman Kozlovskyi <krzroman@gmail.com>
2015-09-28 14:50:00 +0300

    Import barbecue package

---
+++ barbecue/__init__.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from fractions import Fraction
+
+
+def vnmax(features):
+    return sum([max([j['value'] for j in i['enum']]) for i in features])
+
+
+def cooking(price, features=None, parameters=None):
+    """
+    MEAT price normalization
+
+    features = [
+        {
+            "code": "OCDS-123454-AIR-INTAKE",
+            "description": "\u0415\u0444\u0435\u043a\u0442\u0438\u0432\u043d\u0430 \u043f\u043e\u0442\u0443\u0436\u043d\u0456\u0441\u0442\u044c \u0432\u0441\u043c\u043e\u043a\u0442\u0443\u0432\u0430\u043d\u043d\u044f \u043f\u0438\u043b\u043e\u0441\u043e\u0441\u0430,\u0432 \u0432\u0430\u0442\u0430\u0445 (\u0430\u0435\u0440\u043e\u0432\u0430\u0442\u0430\u0445)",
+            "title": "\u041f\u043e\u0442\u0443\u0436\u043d\u0456\u0441\u0442\u044c \u0432\u0441\u043c\u043e\u043a\u0442\u0443\u0432\u0430\u043d\u043d\u044f",
+            "enum": [
+                {
+                    "value": 0.1,
+                    "title": "\u0414\u043e 1000 \u0412\u0442"
+                },
+                {
+                    "value": 0.15,
+                    "title": "\u0411\u0456\u043b\u044c\u0448\u0435 1000 \u0412\u0442"
+                }
+            ],
+            "title_en": "Air Intake",
+            "relatedItem": "123456",
+            "featureOf": "item"
+        },
+        {
+            "code": "OCDS-123454-YEARS",
+            "description": "\u041a\u0456\u043b\u044c\u043a\u0456\u0441\u0442\u044c \u0440\u043e\u043a\u0456\u0432, \u044f\u043a\u0456 \u043e\u0440\u0433\u0430\u043d\u0456\u0437\u0430\u0446\u0456\u044f \u0443\u0447\u0430\u0441\u043d\u0438\u043a \u043f\u0440\u0430\u0446\u044e\u0454 \u043d\u0430 \u0440\u0438\u043d\u043a\u0443",
+            "title": "\u0420\u043e\u043a\u0456\u0432 \u043d\u0430 \u0440\u0438\u043d\u043a\u0443",
+            "enum": [
+                {
+                    "value": 0.05,
+                    "title": "\u0414\u043e 3 \u0440\u043e\u043a\u0456\u0432"
+                },
+                {
+                    "value": 0.1,
+                    "title": "\u0412\u0456\u0434 3 \u0434\u043e 5 \u0440\u043e\u043a\u0456\u0432"
+                },
+                {
+                    "value": 0.15,
+                    "title": "\u0411\u0456\u043b\u044c\u0448\u0435 5 \u0440\u043e\u043a\u0456\u0432"
+                }
+            ],
+            "title_en": "Years trading",
+            "featureOf": "tenderer"
+        }
+    ]
+
+    parameters = [
+        {
+            "code": "OCDS-123454-AIR-INTAKE",
+            "value": 0.1
+        },
+        {
+            "code": "OCDS-123454-YEARS",
+            "value": 0.1
+        }
+    ]
+
+    coef = 1 + Fraction(1, 5) / (1 - Fraction(3, 10)) = Fraction(9, 7)
+    """
+    if not features or not parameters:
+        return price
+    vmax = Fraction(vnmax(features))
+    vn = sum([Fraction(i['value']) for i in parameters])
+    coef = 1 + vn / (1 - vmax)
+    return Fraction(price) * coef
+
+
+def chef(bids, features=None, ignore=[]):
+    """
+    MEAT bids sorting
+    """
+    if not features:
+        sorted_bids = sorted(bids, key=lambda i: (i['value']['amount'], i['date']))
+    else:
+        vmax = Fraction(vnmax(features))
+        sorted_bids = sorted(bids, key=lambda i: (Fraction(i['value']['amount']) * (1 + sum([Fraction(j['value']) for j in i['parameters']]) / (1 - vmax)), i['date']))
+    return [i for i in sorted_bids if i['id'] not in ignore]
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='barbecue',
+      version=version,
+      description="MEAT price normalization",
+      long_description="""\
+""",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+      ],
+      keywords='',
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      url='https://github.com/openprocurement/barbecue',
+      license='Apache License 2.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
